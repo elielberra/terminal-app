862515ab458e907d45c5b66ff0f77802705ad67b
Eli <berraeliel@gmail.com>
2025-09-27 00:08:31 -0300

    chores: comments and old line removals

--- up.sh
+++ up.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
 
+# Remove pre existing containers
 docker compose down --remove-orphans
 
+# Build the Go App
 cd terminal-app
 go build -o terminal-app main.go
-source .env.dev
 cd ..
 
+# Reload the apparmor profile
 if ! sudo apparmor_parser -r -W /etc/apparmor.d/terminal-app-compiled; then
   echo "❌ Failed to reload AppArmor profile. Aborting."
   exit 1
 fi
 
+# Spawn new containers
 docker compose up
