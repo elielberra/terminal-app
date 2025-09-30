3fa77fb137e2887de5d8c212a70a856c515a8989
Eli <berraeliel@gmail.com>
2025-09-29 23:34:17 -0300

    chores: del old files

--- scripts/time.sh
+++
@@ -1,7 +0,0 @@
-start_script_time=$(date +%s%3N)   # take start time in ms
-
-timeout 2.5s bash scripts/rain2.sh 
-
-end_script_time=$(date +%s%3N)     # take end time in ms
-
-echo $((end_script_time - start_script_time)) "ms"   # this is the runtime in ms
