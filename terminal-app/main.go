62f28435c00e62b05565f0ee159566a318c8857c
Eli <berraeliel@gmail.com>
2025-10-10 00:12:50 -0300

    fix: readjust pty cols and rows automatically to browser window

--- terminal-app/main.go
+++ terminal-app/main.go
@@ -57,7 +57,8 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	userLanguage := getUserLanguage(r)
 	defer conn.Close()
-	cmd := exec.Command("/bin/rbash")
+	cmd := exec.Command("/bin/bash")
+	// cmd := exec.Command("/bin/rbash")
 	cmd.Env = append(os.Environ(),
 		"LANG="+string(userLanguage),
 	)
