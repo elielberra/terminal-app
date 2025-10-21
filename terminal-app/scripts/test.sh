d6f6851d25066a039a60a1c541ec5c063781abd1
Eli <berraeliel@gmail.com>
2025-10-21 18:16:53 -0300

    feat: typewriter with more sound effect

--- terminal-app/scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-text="Hello there, welcome to my terminal!"
-for ((i=0; i<${#text}; i++)); do
-  printf "%s" "${text:$i:1}"
-  sleep 0.03   # delay between letters (in seconds)
-done
-echo
