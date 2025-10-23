edb2719a8606a2e90f39a0e0f75c3fcf7913d682
Eli <berraeliel@gmail.com>
2025-10-22 23:19:01 -0300

    fix: minor fixes

--- terminal-app/scripts/welcome-msg.sh
+++ terminal-app/scripts/welcome-msg.sh
@@ -11,7 +11,7 @@ RESET="\033[0m"
 WELCOME_MSG_ENG="Hello and welcome to my webpage!
 
 If you’re wondering why you’re seeing a terminal instead of a UI, it’s because I’m not great at designing things.
-The last time I tried to use JavaScript I had a severe allergic reaction and my doctor told me to stay away from frontend frameworks.
+The last time I tried to use JavaScript I had a severe allergic reaction and my doctor told me to stay away from frontend development.
 Besides, I think this is an interesting way for people to get to know who I am.
 
 Like I said before, you have to type some commands in the terminal to use this webpage.
