ccd13d6b82c855ca9f2a042356a11b952a1cfb2b
Eli <berraeliel@gmail.com>
2025-10-22 23:04:20 -0300

    chores: removed deprecated iptables rules

--- iptables-rules.sh
+++ iptables-rules.sh
@@ -5,12 +5,3 @@ CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddres
 
 # Block all outbound traffic from the ip of terminal-app
 sudo iptables -I DOCKER-USER -s $CONTAINER_IP -j DROP -m comment --comment "block terminal-app outbound connections"
-
-# # Allow inbound traffic only to port 8080
-# sudo iptables -I DOCKER-USER -d $CONTAINER_IP -p tcp --dport 8080 -j ACCEPT "allpw terminal-app inbound tcp connections on :8080"
-
-# # Drop all other inbound traffic
-# sudo iptables -I DOCKER-USER -d $CONTAINER_IP -j DROP "block terminal-app inbound connections"
-
-## Remove firewall rules
-# sudo iptables -F DOCKER-USER
