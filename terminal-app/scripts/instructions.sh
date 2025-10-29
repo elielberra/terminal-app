5aae648fda3444c24d24018b92b1623e94e18847
Eli <berraeliel@gmail.com>
2025-10-29 00:23:42 +0000

    fix: curious visitor txt

--- terminal-app/scripts/instructions.sh
+++ terminal-app/scripts/instructions.sh
@@ -4,7 +4,7 @@ RESET="\033[0m"
 GREEN="\033[0;32m"
 CYAN="\033[1;36m"
 
-INSTRUCTIONS_ENG="- If you’re a ${CYAN}random user${RESET}, type ${GREEN}eliel champion${RESET}. "\
+INSTRUCTIONS_ENG="- If you’re a ${CYAN}curious visitor${RESET}, type ${GREEN}eliel champion${RESET}. "\
 "I have a surpise prepared for you. "\
 "First, a super ultra HD video will be displayed (you’ll need a powerful GPU to render those graphics). Second... France.
 
@@ -23,7 +23,7 @@ If you want to see the manual with the instructions, enter the command ${GREEN}e
 You can mute the background server noises and switch to a light theme using the two buttons in the top right corner.
 "
 
-INSTRUCTIONS_ESP="- Si sos un ${CYAN}usuario random${RESET}, escribí ${GREEN}eliel champion${RESET}. "\
+INSTRUCTIONS_ESP="- Si sos un ${CYAN}visitante curioso${RESET}, escribí ${GREEN}eliel champion${RESET}. "\
 "Tengo una sorpresa preparada para vos. "\
 "Primero, se va a mostrar un video en super ultra HD (vas a necesitar una muy buena GPU para renderizar esos gráficos). "\
 "Segundo... Francia.
