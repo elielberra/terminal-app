08301c08ec2f16c23ce2faca357e7d175d2db785
Eli <berraeliel@gmail.com>
2025-11-02 16:15:40 +0000

    fix: welcome msg

--- terminal-app/scripts/welcome-msg.sh
+++ terminal-app/scripts/welcome-msg.sh
@@ -8,16 +8,16 @@ sleep 0.001
 
 WELCOME_MSG_ENG="Hello and welcome to my webpage!
 
-If you’re wondering why you’re seeing a terminal instead of a UI, it’s because I’m not great at designing things "\
-"and I think this is an interesting way for people to get to know who I am. "\
+If you’re wondering why you’re seeing a terminal instead of a UI, it is because "\
+"I thought this would be an interesting way for people to get to know who I am. "\
 "The commands you can use to interact with this webpage are listed below and highlighted in green. "\
 "Type them exactly as shown and press Enter to execute them:
 "
 
 WELCOME_MSG_ESP="¡Hola y bienvenid@ a mi página web!
 
-Si te estás preguntando por qué ves una terminal en lugar de una interfaz, es porque no soy muy bueno diseñando cosas. "\
-"y me parece una forma alternativa interesante para que la gente me conozca un poco más. "\
+Si te estás preguntando por qué ves una terminal en lugar de una interfaz, es porque "\
+"me pareció que sería una forma alternativa interesante para que la gente me conozca un poco más. "\
 "Los comandos que podés usar están detallados acá abajo y coloreados en verde. "\
 "Escribilos exactamente como están y presioná Enter para ejecutarlos:
 "
