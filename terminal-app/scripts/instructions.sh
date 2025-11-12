235786ae19779aae4b2f6282c08d134eafaa4c37
Eli <berraeliel@gmail.com>
2025-11-12 00:39:46 +0000

    chores: txt removal

--- terminal-app/scripts/instructions.sh
+++ terminal-app/scripts/instructions.sh
@@ -6,7 +6,7 @@ CYAN="\033[1;36m"
 
 INSTRUCTIONS_ENG="- If you’re a ${CYAN}curious visitor${RESET}, type ${GREEN}eliel chatbot${RESET} to ask me questions using AI. "\
 "I also have a surprise prepared for you, type ${GREEN}eliel champion${RESET} to watch it. "\
-"First, a super ultra HD video will be displayed (you’ll need a powerful GPU to render those graphics). Second... France.
+"First, a super ultra HD video will be displayed. Second... France.
 
 - If you’re a ${CYAN}recruiter${RESET}, run ${GREEN}eliel cv${RESET}. "\
 "This opens the latest PDF version of my CV in English by default. For Spanish, run ${GREEN}eliel cv --language esp${RESET}.
@@ -24,7 +24,7 @@ If you want to see the manual with this instructions again, enter the command ${
 
 INSTRUCTIONS_ESP="- Si sos un ${CYAN}visitante curioso${RESET}, escribí ${GREEN}eliel chatbot${RESET} para hacerme preguntas usando IA. "\
 "También una sorpresa preparada para vos, para verla escribí ${GREEN}eliel champion${RESET}. "\
-"Primero, se va a mostrar un video en super ultra HD (vas a necesitar una muy buena GPU para renderizar esos gráficos). "\
+"Primero, se va a mostrar un video en super ultra HD. "\
 "Segundo... Francia.
 
 - Si sos un ${CYAN}recruiter${RESET}, ejecutá ${GREEN}eliel cv${RESET}. "\
